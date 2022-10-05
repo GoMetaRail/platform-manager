8da688bc92e46506950cce25c3f95d20ce65ac89
Chris Augustine <chriscalifornia88@gmail.com>
2022-10-05 12:32:09 -0700

    Added category search typeahead

--- src/ui/CategoryPicker.js
+++ src/ui/CategoryPicker.js
@@ -1,38 +1,109 @@
-import React from 'react';
-import SelectSearch from "react-select-search";
+import React, {useState, useEffect} from 'react';
 import 'react-select-search/style.css';
-import {SearchField} from "@aws-amplify/ui-react";
+import {searchCategories} from "../graphql/queries";
+import {API, graphqlOperation} from "aws-amplify";
+import {Collection, Card, Link} from "@aws-amplify/ui-react";
 
 function CategoryPicker() {
-  const options = [
-    {name: 'Swedish', value: 'sv'},
-    {name: 'English', value: 'en'},
-    {
-      type: 'group',
-      name: 'Group name',
-      items: [
-        {name: 'Spanish', value: 'es'},
-      ]
-    },
-  ];
+  const [options, setOptions] = useState([]);
+  const [selected, setSelected] = useState([]);
+  const Typeahead = require('react-typeahead').Typeahead;
+  let searchTimeout = null;
+
+  useEffect(() => {
+  }, []);
+
+  function searchItems(q) {
+    clearTimeout(searchTimeout);
+    searchTimeout = setTimeout(async () => {
+      try {
+        const apiData = await API.graphql(graphqlOperation(searchCategories, {
+          limit: 3,
+          filter: {
+            name: {wildcard: `*${q.toLowerCase()}*`}
+          }
+        }));
+        setOptions(apiData.data.searchCategories.items);
+      } catch (e) {
+        console.error(e);
+        setOptions([]);
+      }
+    }, 250);
+  }
+
+  function selectItem(i) {
+    setSelected(current => {
+      return [
+        ...current,
+        i
+      ];
+    });
+  }
+
+  function deselectItem(id) {
+    setSelected(current => {
+      return current.filter(i => i.id !== id);
+    });
+  }
+
+  function filterItem(search, option) {
+    const selectedIds = selected.map(i=>i.id);
+    return !selectedIds.includes(option.id);
+  }
 
   return (
-    <SearchField
-      label="Search"
-      placeholder="Search here..."
-      autoComplete="false"
-      onSubmit={(q) => {
-        alert(q);
-      }}
-    />
-
-    /*<SelectSearch
-        options={options}
-        value="sv"
-        name="language"
-        placeholder="Choose your language"
-      />*/
+    <div>
+      <label className="amplify-label">Category</label>
+      <div>
+        <Typeahead
+          onOptionSelected={selectItem}
+          options={options}
+          onChange={(e) => searchItems(e.target.value)}
+          onBlur={(e) => { setOptions([]) }}
+          filterOption={filterItem}
+          displayOption={"name"}
+          maxVisible={3}
+          customClasses={{
+            input: "typeahead-input amplify-input amplify-field-group__control",
+            results: "typeahead-results amplify-flex amplify-collection-items",
+            listItem: "typeahead-list-item amplify-card amplify-card--outlined",
+            listAnchor: "amplify-link",
+            // hover: "",
+            // typeahead: "",
+            // resultsTruncated: ""
+          }}
+        />
+      </div>
+      <Card>
+        {
+          selected.length !== 0 && (
+            <Collection
+              items={selected}
+              type="list"
+              direction="row"
+            >
+              {(item, index) => (
+                <Card
+                  key={item.id}
+                  borderRadius="medium"
+                  maxWidth="20rem"
+                  variation="outlined"
+                >
+                  {item.name}
+                  <Link
+                    onClick={(e) => { deselectItem(item.id) }}
+                  >
+                    &nbsp;x
+                  </Link>
+                </Card>
+              )}
+            </Collection>
+          )
+        }
+      </Card>
+    </div>
   );
+
 }
 
 export default CategoryPicker;
