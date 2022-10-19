30988dea41d55fb496f383bdbf6d50b24cc5e895
Chris Augustine <chriscalifornia88@gmail.com>
2022-10-19 17:33:24 -0500

    Fixed issue with adding tags and updating images

--- src/ui/ModelPicker.js
+++ src/ui/ModelPicker.js
@@ -17,6 +17,7 @@ function ModelPicker(props, ref) {
 
   useEffect(() => {
     if (value) {
+      console.log('value', value);
       let tmpValue = value;
       if (!Array.isArray(tmpValue)) {
         tmpValue = [value];
